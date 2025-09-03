a9616bbfe4a739ea345f2c1327c28ac065915dee
Dinesh45kumer <dineshepd001@gmail.com>
2025-09-03 18:20:58 +0530

    dinesh

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>
+        Hello welcome
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et mollitia nulla molestiae aspernatur atque. Maxime sequi enim aliquid cumque dolores?
+      </p>
+    </>
+  )
+}
+
+export default App
